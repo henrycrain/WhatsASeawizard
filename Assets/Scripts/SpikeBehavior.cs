a2b293a80af6162b64ddf91802677c3d693567f4
Kyle Simon <Kyle.Simon@stonybrook.edu>
2019-12-11 18:13:25 -0500

    Added spike behavior, placed enemies in first zone

---
+++ Assets/Scripts/SpikeBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeBehavior : MonoBehaviour
+{
+   private void OnTriggerEnter(Collider other)
+    {
+        var healthOfTarget = other.GetComponent<Damageable>();
+        if (healthOfTarget != null)
+        {
+            // Kill the thing falling on the spikes
+            healthOfTarget.Damage(float.PositiveInfinity);
+        }
+    }
+}

17367c714e6c8a126d1f64a6ce97123e3fc245f3
hcrain <henry.crain@stonybrook.edu>
2019-12-18 17:29:24 -0500

    Added sound

---
+++ Assets/Scripts/PlayerSoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerSoundManager : MonoBehaviour
+{
+    private AudioSource source;
+    
+    private void Start()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+        source.loop = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Vertical") > 0 && !source.isPlaying)
+        {
+            source.Play();
+        }
+        else if (Math.Abs(Input.GetAxis("Vertical")) < 0.001f && source.isPlaying)
+        {
+            source.Stop();
+        }
+    }
+}
